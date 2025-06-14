47812dba6701d6b7b3c9f791fe67b80fb0c6c885
SAQIB SK <46896553+saqibskvip@users.noreply.github.com>
2025-06-14 08:49:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 // Initialize Firebase
 admin.initializeApp({
